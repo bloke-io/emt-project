bec32a86a0a920686c0264c454714a72c14cdf00
Vlado Dimovski <vladodimovski99@gmail.com>
2023-09-03 23:01:32 +0200

    Initial api setup

---
+++ backend/emt-be/src/api/science-paper/controllers/science-paper.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * science-paper controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::science-paper.science-paper');
---
+++ backend/emt-be/src/api/science-paper/routes/science-paper.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * science-paper router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::science-paper.science-paper');
