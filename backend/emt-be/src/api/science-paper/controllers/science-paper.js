37f3ef700d45ebf4396c45b6bef4dded39e1a03d
Vlado Dimovski <vladodimovski99@gmail.com>
2023-09-04 21:27:32 +0200

    Initial next.js setup and small restructure

--- backend/emt-be/src/api/science-paper/controllers/science-paper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * science-paper controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::science-paper.science-paper');
--- backend/emt-be/src/api/science-paper/services/science-paper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * science-paper service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::science-paper.science-paper');
