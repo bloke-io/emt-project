fe312a200d78e7da6523e48ebe3970c94c4e1b33
Vlado Dimovski <vladodimovski99@gmail.com>
2023-09-04 21:28:49 +0200

    Files push

---
+++ backend/src/api/science-paper/services/science-paper.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * science-paper service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::science-paper.science-paper');
